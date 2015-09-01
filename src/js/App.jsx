539d6affafd94f7c1308573f261b5429d6b081db
Raphaël Benitte <raphael.benitte@fullsix.com>
2015-09-01 11:03:34 +0200

    init components

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import './../css/app.styl';
+import React    from 'react';
+import Header   from './components/Header.jsx';
+import Controls from './components/Controls.jsx';
+import Content  from './components/Content.jsx';
+import Footer   from './components/Footer.jsx';
 
-require('./../css/app.styl');
 
 const App = React.createClass({
     render() {
         return (
             <div>
+                <Header/>
+                <Controls/>
+                <Content/>
+                <Footer/>
             </div>
         );
     }
 });
+
 
 React.render(<App/>, document.getElementById('app'));
---
+++ src/js/components/Content.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class Content extends Component {
+    render() {
+        return null;
+    }
+}
+
+
+export { Content as default };
